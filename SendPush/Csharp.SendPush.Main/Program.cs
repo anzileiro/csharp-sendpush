c0471ac450f1f640f67668830da5056f9ef14651
anzileiro <anderson.anzileiro@gmail.com>
2014-11-18 12:29:29 -0200

    first commit, sending push for Windows Phone 8

---
+++ SendPush/Csharp.SendPush.Main/Program.cs
@@ -0,0 +1,15 @@
+﻿using Csharp.SendPush.Model;
+
+namespace Csharp.SendPush.Main
+{
+    public class Program
+    {
+        public const string WP_DEVICE_KEY_TEST = "YOUR_DEVICE_JEY";
+
+        public static void Main(string[] args)
+        {
+            var wp = new WindowsPhone(WP_DEVICE_KEY_TEST, "your message !!!", "yourViewInApp.xaml", TypeShipping.DeliverImmediate);
+            wp.Send();
+        }
+    }
+}
---
+++ SendPush/Csharp.SendPush.Main/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Csharp.SendPush.Main")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Csharp.SendPush.Main")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0f99afe8-2dab-423f-9ecd-b5519d02ab70")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
