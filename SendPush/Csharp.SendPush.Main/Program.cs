c27dd852d1a39b7b2243e39adbe0c73b6e9166d2
anzileiro <anderson.anzileiro@gmail.com>
2014-11-19 15:01:41 -0200

    sending push for android with GCM :D

--- SendPush/Csharp.SendPush.Main/Program.cs
+++ SendPush/Csharp.SendPush.Main/Program.cs
@@ -4,12 +4,16 @@ namespace Csharp.SendPush.Main
 {
     public class Program
     {
-        public const string WP_DEVICE_KEY_TEST = "YOUR_DEVICE_KEY";
+        public const string WINDOWS_PHONE_DEVICE_KEY_TEST = "YOUR_DEVICE_KEY";
+        public const string ANDROID_DEVICE_KEY_TEST = "YOUR_DEVICE_KEY";
 
         public static void Main(string[] args)
         {
-            var wp = new WindowsPhone(WP_DEVICE_KEY_TEST, "yourMessage !!!", "yourViewInApp.xaml", TypeShipping.DeliverImmediate);
-            wp.Send();
+            var windowsPhone = new WindowsPhone(WINDOWS_PHONE_DEVICE_KEY_TEST, "your Message !!!", "yourViewInApp.xaml", TypeShipping.DeliverImmediate);
+            windowsPhone.Send();
+
+            var android = new Android(ANDROID_DEVICE_KEY_TEST, "your Message !!!", "yourCollapseKey");
+            android.Send();
         }
     }
 }
---
+++ SendPush/Csharp.SendPush.Model/Android.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Text;
+
+namespace Csharp.SendPush.Model
+{
+    public sealed class Android : Device
+    {
+        public const string API_GCM = "https://android.googleapis.com/gcm/send";
+        public const string API_KEY = "key=your-key";
+        public const string API_ID = "id=your-id";
+
+        public string CollapseKey { get; set; }
+        public byte[] Push { get; set; }
+
+        public Android(string deviceKey, string message, string collapseKey)
+        {
+            this.Key = deviceKey;
+            this.Message = message;
+            this.CollapseKey = collapseKey;
+        }
+
+        public async void Send()
+        {
+            var json = JObject.Parse("{'registration_ids':['" + this.Key + "'],'collapse_key':'" + this.CollapseKey + "','data':{'message':'" + this.Message + "'}}").ToString();
+
+            Push = Encoding.UTF8.GetBytes(json);
+
+            WebRequest webRequest = Request.Send(API_GCM, this.Push.Length, new Header[]
+            {
+                new Header 
+                { 
+                    Name = "Authorization",
+                    Value = API_KEY
+                },
+                new Header 
+                { 
+                    Name = "Sender",
+                    Value = API_ID
+                }
+            }, contentType: "application/json");
+
+            Stream.Send(webRequest, this.Push);
+
+            await Response.GetAsync(webRequest);
+        }
+    }
+}
